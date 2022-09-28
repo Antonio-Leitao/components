4d998caa69d601217f246176f310549fb6346f07
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2022-09-28 18:34:18 +0200

    fixed scroll animations to use svelte's animation directive, Fkn genious IMO

--- src/lib/ScrollComponent.svelte
+++ src/lib/ScrollComponent.svelte
@@ -1,10 +1,12 @@
 <script>
   import { inView } from "./scripts/inview.js";
+  import { fly } from "svelte/transition";
+  import { quintOut } from "svelte/easing";
 
   let visible;
 </script>
 
-<div
+<!-- <div
   class:hidden={!visible}
   use:inView={{ threshold: 0.5 }}
   on:enter={() => (visible = true)}
@@ -20,5 +22,34 @@
   .hidden {
     opacity: 0;
     transform: translateX(-100%);
+  }
+</style> -->
+
+{#key visible}
+  {#if visible}
+    <div
+      in:fly={{ duration: 300, x: -100, opacity: 0.5, easing: quintOut }}
+      class:hidden={!visible}
+      use:inView={{ threshold: 0.5 }}
+      on:enter={() => (visible = true)}
+      on:leave={() => (visible = false)}
+    >
+      <slot />
+    </div>
+  {:else}
+    <div
+      class:placeholder={!visible}
+      use:inView={{ threshold: 0.5 }}
+      on:enter={() => (visible = true)}
+      on:leave={() => (visible = false)}
+    >
+      <slot />
+    </div>
+  {/if}
+{/key}
+
+<style>
+  .placeholder {
+    visibility: hidden;
   }
 </style>
